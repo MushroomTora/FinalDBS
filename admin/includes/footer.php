9c53176553aa31fae42997c8bc0d72b2afff5a0e
ThuHuong <nammhuongg1007@gmail.com>
2020-11-17 21:53:29 +0700

    $'Init'

---
+++ admin/includes/footer.php
@@ -0,0 +1,4 @@
+
+                <footer class="footer text-right">
+                   Developed by DuongChungLinh 5C16 AIWFINAL.
+                </footer>
---
+++ includes/footer.php
@@ -0,0 +1,6 @@
+    <footer class="py-5 bg-dark">
+      <div class="container">
+        <p class="m-0 text-center text-white">Copyright &copy; DuongChungLinh 5C16 AIWFINAL</p>
+      </div>
+      <!-- /.container -->
+    </footer>
